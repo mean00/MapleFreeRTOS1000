117aec896c6ad78bfc94d5fc20d3c093a2224f96
mean <fixounet@free.fr>
2021-01-22 10:13:50 +0100

    update to 2020 TLS with M3duino & M4FPUduino

---
+++ Source/portable/GCC/stm32duino/stm32duino.c
@@ -0,0 +1,27 @@
+/*
+
+ */
+
+/*-----------------------------------------------------------
+* Implementation of functions defined in portable.h for the ARM CM3 port.
+*----------------------------------------------------------*/
+
+/* Scheduler includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+
+/*-----------------------------------------------------------*/
+// MEANX
+extern    void do_assert(const char *a);
+void vApplicationStackOverflowHook(TaskHandle_t pxTask,
+                                   char *pcTaskName) {
+    /* This function will get called if a task overflows its stack.
+     * If the parameters are corrupt then inspect pxCurrentTCB to find
+     * which was the offending task. */
+
+    (void) pxTask;
+    (void) pcTaskName;
+    do_assert("stackOverFlow");
+           ;
+}
+
